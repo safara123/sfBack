822214574657d229d4e2e01837d6894656324b18
Hussein Hmede <h.hmede@equadis.com>
2023-05-29 17:51:40 +0300

    last

--- routes/admin.js
+++ routes/admin.js
@@ -66,10 +66,11 @@ router.post(
 router.get("/getAllFolders",
     VerifyAdminToken,
     async function (req, res, next) {
-        await Folder.find()
+        const drawerId = req.query.drawerId;
+        await Folder.find({ drawer: drawerId })
+            .populate("drawer")
             .then((user) => res.json(user))
             .catch((err) => res.status(400).json("Error: " + err));
-
     });
 
 
